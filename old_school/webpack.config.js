c4ce186cce6c540ba283b28e9f9fb5cd0dfbba61
shjang7 <lucy.sh.jang@gmail.com>
2019-10-02 20:52:46 +0900

    Update directory

--- old_school/webpack.config.js
+++ old_school/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/application.js',
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
